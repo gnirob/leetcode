466c81139590b53ff62477db3860faf6a7c6d4e9
Boring <boring@boring.wang>
2019-04-21 18:24:23 +0800

    Add 171

---
+++ dist/171-excel-sheet-column-number.js
@@ -0,0 +1,11 @@
+const titleToNumber = (s) => {
+    let result = 0;
+    const charCodeStart = 'A'.charCodeAt(0);
+    const radix = 26;
+    for (let i = 0; i < s.length; i++) {
+        result +=
+            (s[s.length - 1 - i].charCodeAt(0) - charCodeStart + 1) *
+            radix ** i;
+    }
+    return result;
+};
