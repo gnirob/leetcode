6a711a6d4af4038323edf6a9aa898141be3d7c84
Boring <boring@boring.wang>
2019-04-21 10:35:35 +0800

    Add 169

---
+++ dist/169-majority-element.js
@@ -0,0 +1,3 @@
+const majorityElement = (nums) => {
+    return nums.sort()[Math.floor(nums.length / 2)];
+};
---
+++ src/169-majority-element.ts
@@ -0,0 +1,3 @@
+const majorityElement = (nums: number[]): number => {
+    return nums.sort()[Math.floor(nums.length / 2)];
+};
