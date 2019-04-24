866529a85a0e211d62e15732b50eb6f09059906f
Boring <boring@boring.wang>
2019-04-24 11:14:32 +0800

    Add 118

---
+++ dist/118-pascals-triangle.js
@@ -0,0 +1,14 @@
+const generate = (numRows) => {
+    const result = [];
+    for (let i = 1; i <= numRows; i++) {
+        const row = Array.from({length: i}, (item, index) => {
+            if ([0, i - 1].includes(index)) {
+                return 1;
+            }
+            const prevRow = result[i - 2];
+            return prevRow[index - 1] + prevRow[index];
+        });
+        result.push(row);
+    }
+    return result;
+};
