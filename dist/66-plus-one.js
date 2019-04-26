83b7423918247e31ad57e1148759511ca2292d93
Boring <boring@boring.wang>
2019-04-26 16:51:09 +0800

    Add 66

---
+++ dist/66-plus-one.js
@@ -0,0 +1,20 @@
+const plusOne = (digits) => {
+    let extra = 1;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (!extra) {
+            break;
+        }
+        const newValue = digits[i] + extra;
+        if (newValue <= 9) {
+            digits[i] = newValue;
+            extra = 0;
+        } else {
+            digits[i] = 0;
+            extra = 1;
+        }
+    }
+    if (extra) {
+        digits.unshift(1);
+    }
+    return digits;
+};
